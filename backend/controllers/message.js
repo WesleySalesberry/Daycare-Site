ec0fcb8cccb82eb9ca225587b06d20278deba7e9
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2022-07-14 07:17:56 -0400

    Backend (#1)
    * Added database
    
    * Added database connector
    
    * Renamed schema
    
    * Updated the routes

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -1,13 +1,20 @@
+import { Message } from '../models/Message.js'
+
 /**
  * @desc    Creates Messages
  * @route   GET /api/v1/message
  * @access  Public
  */
 export const createMessages = async (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: "Message"
-  })
+  try {
+    await Message.create(req.body)
+    res.status(200).json({
+      success: true,
+      data: "Your message has been sent!"
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 /**
@@ -16,9 +23,11 @@ export const createMessages = async (req, res, next) => {
  * @access  Admin
  */
 export const getMessages = async (req, res, next) => {
+  const messages = await Message.find({})
   res.status(200).json({
     success: true,
-    data: "Message"
+    count: messages.length,
+    data: messages
   })
 }
 
@@ -28,9 +37,21 @@ export const getMessages = async (req, res, next) => {
  * @access  Admin
  */
 export const deleteMessages = async (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: "Delete Message"
-  })
+  try {
+    const message = await Message.findById(req.params.id)
+    if(!message){
+      res.status(401)
+      throw new Error('Message not found')
+    }
+
+    await message.remove()
+
+    res.status(200).json({
+      success: true,
+      data: "Message Removed"
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
