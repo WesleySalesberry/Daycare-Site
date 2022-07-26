89bd4accce8c7606ec4fb5a1632df894bc66f235
WesleySalesberry <61122432+WesleySalesberry@users.noreply.github.com>
2022-07-25 20:07:21 -0400

    Frontend (#14)
    * Added database
    
    * Added database connector
    
    * Renamed schema
    
    * Updated the routes
    
    * Renamed a file
    
    * Moved logic to a component
    
    * Added style to display point for element
    
    * Removed console log
    
    * Comment out a link
    
    * Create the display page
    
    * Updated the message card
    
    * Updated the the form
    
    * Updated the the name
    
    * Updated Nth-check
    
    * Added the routes to get message count
    
    * Updated frontend
    
    * Removed file
    
    * Started the file down load links

--- frontend/src/utils/paths.js
+++ frontend/src/utils/paths.js
@@ -5,3 +5,9 @@ export const USER = 'user'
 export const REGISTER = 'register'
 
 export const DASHBOARD = 'dashboard'
+
+export const DOWNLOAD = "download"
+export const APPLICATION = "application"
+export const SUMMARY_OF_THE_LAW = "summary_of_the_law"
+export const TRANSPORTATION_PERMISSON = "transportation_permisson"
+export const DENTAL_SCREENING = "dental_screening"
